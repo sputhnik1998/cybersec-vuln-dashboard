927aac142268141f89f76d8bd51833b9698f7ddd
Sputhnik18 <sputhnikiwnl@gmail.com>
2025-10-29 20:39:54 -0500

    Day0: Project and filestructure

---
+++ cybersec-vuln-dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/common/header/header'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ cybersec-vuln-dashboard/src/components/common/header/header.tsx
@@ -0,0 +1,12 @@
+import { Paper } from "@mui/material";
+
+const Header = ()=>{
+    return <div>
+        <h2>CyberSecurity Vuln Dashboard</h2>
+        <Paper />
+   
+    </div>
+    
+}
+
+export default Header;
